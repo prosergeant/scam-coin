b95c7c42d1c83142174114c71872e01fce6a8ef5
prosergeant <sberendeyev@gmail.com>
2024-01-26 00:37:07 +0600

    new tests

--- backend/src/database.test.js
+++ backend/src/database.test.js
@@ -7,28 +7,39 @@ beforeAll(async () => {
 test('create person', async () => {
     expect.assertions(1);
     const person = await db.Person.create({
-        id: 1,
-        firstName: 'Bobbie',
-        lastName: 'Draper'
+        // id: 1,
+        username: 'test',
+        userid: '123456'
     });
-    expect(person.id).toEqual(1);
+    expect(person.username).toEqual('test');
 });
 
 test('get person', async () => {
     expect.assertions(2);
-    const person = await db.Person.findByPk(1);
-    expect(person.firstName).toEqual('Bobbie');
-    expect(person.lastName).toEqual('Draper');
+    const person = await db.Person.findOne({
+        where: {
+            username: 'test',
+            userid: '123456'
+        }
+    });
+    expect(person.username).toEqual('test');
+    expect(person.userid).toEqual('123456');
 });
 
 test('delete person', async () => {
     expect.assertions(1);
     await db.Person.destroy({
         where: {
-            id: 1
+            username: 'test',
+            userid: '123456'
         }
     });
-    const person = await db.Person.findByPk(1);
+    const person = await db.Person.findOne({
+        where: {
+            username: 'test',
+            userid: '123456'
+        }
+    });
     expect(person).toBeNull();
 });
 
