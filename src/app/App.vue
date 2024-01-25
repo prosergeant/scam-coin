09ee5ae36ebca5caed76e5ad4e345922ab0140e5
prosergeant <sberendeyev@gmail.com>
2024-01-25 14:52:17 +0600

    add styles

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,7 +5,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { DefaultLayout } from '@/widgets/default-layout/'
-import { computed, onMounted } from 'vue';
+import { computed, onMounted } from 'vue'
 
 const route = useRoute()
 
@@ -25,32 +25,32 @@ const layout = computed(() => route.meta.layout || DefaultLayout)
 // })
 
 onMounted(async () => {
-	if(window) {
-		document.addEventListener('gesturestart', function(e) {
-			e.preventDefault();
-			//@ts-ignore
-			document.body.style.zoom = 1;
-		});
+    if (window) {
+        document.addEventListener('gesturestart', function (e) {
+            e.preventDefault()
+            //@ts-ignore
+            document.body.style.zoom = 1
+        })
 
-		document.addEventListener('gesturechange', function(e) {
-			e.preventDefault();
-			//@ts-ignore
-			document.body.style.zoom = 1;
-		});
+        document.addEventListener('gesturechange', function (e) {
+            e.preventDefault()
+            //@ts-ignore
+            document.body.style.zoom = 1
+        })
 
-		document.addEventListener('gestureend', function(e) {
-			e.preventDefault();
-			//@ts-ignore
-			document.body.style.zoom = 1;
-		});
+        document.addEventListener('gestureend', function (e) {
+            e.preventDefault()
+            //@ts-ignore
+            document.body.style.zoom = 1
+        })
 
-		document.ondblclick = function(e) {
-			e.preventDefault();
-		}
-	}
+        document.ondblclick = function (e) {
+            e.preventDefault()
+        }
+    }
 })
 </script>
 
 <style lang="scss">
-//@import 'index.css';
+@import 'index.scss';
 </style>
--- src/pages/home/ui/Home.vue
+++ src/pages/home/ui/Home.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="main">
+        <div class="space" />
         <h3>Coins: {{ coins }}</h3>
         <img
             src="/coin.png"
             alt=""
-						@click='coins++'
+            @click="clickCoin"
+            :class="{ clicked: rotation }"
         />
     </div>
 </template>
@@ -12,21 +14,53 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const rotation = ref(false)
+const clickCoin = () => {
+    rotation.value = true
+    setTimeout(() => {
+        rotation.value = false
+        coins.value++
+    }, 100)
+}
+
 const coins = ref(0)
 </script>
 
 <style scoped lang="scss">
+h3 {
+    font-size: 30px;
+    font-weight: 700;
+    color: white;
+}
+
 .main {
     width: 100%;
     height: 100dvh;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    //justify-content: center;
     align-items: center;
+
+    background: currentColor url('/background.png') no-repeat top;
+    background-size: contain;
 }
 
 img {
     width: 240px;
     height: 240px;
+    position: absolute;
+    left: 43.2%;
+    top: 42.2%;
+    transition: all ease 200ms;
+    filter: drop-shadow(2px 4px 6px black);
+    border-radius: 50%;
+
+    &.clicked {
+        transform: rotate3d(1, 1, 0, 30deg);
+    }
+}
+
+.space {
+    height: 15dvh;
 }
 </style>
