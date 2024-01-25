972ce7f3daa2c2afebfa077da59a8b2e23b95c7d
prosergeant <sberendeyev@gmail.com>
2024-01-25 20:07:56 +0600

    change url for database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,7 +3,7 @@ const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
     process.env.DB_USER || 'postgres',
     process.env.DB_PASSWORD || '',
     {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || '5.35.89.195',
         port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         dialectOptions: {
