81344adedcba2b77908d957ae58be1e6179dacbf
prosergeant <sberendeyev@gmail.com>
2024-01-26 18:27:16 +0600

    test animation 2

--- frontend/src/widgets/coin/ui/Coin.vue
+++ frontend/src/widgets/coin/ui/Coin.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="main">
-        <h3
-            class="coins-count"
-            :style="`--num:${coins}`"
-        ></h3>
+        <h3 class="coins-count">Coins: {{ coins }}</h3>
         <div style="height: 50px" />
         <div class="background-container">
             <img
