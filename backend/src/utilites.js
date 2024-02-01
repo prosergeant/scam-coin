95d1c94917aa25073302dc59704a12e15a83b4cc
prosergeant <sberendeyev@gmail.com>
2024-02-01 15:22:50 +0600

    log for ws 8

--- backend/src/utilites.js
+++ backend/src/utilites.js
@@ -44,7 +44,33 @@ const httpsGetData = (url, params) => {
 }
 
 const httpsPostData = (url, body) => {
-    const postData = JSON.stringify(body)
+    // const postData = JSON.stringify(body)
+    // const options = {
+    //     hostname: 'bronkz.app',
+    //     port: 443,
+    //     path: `/api-ws${url}`,
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Content-Length': Buffer.byteLength(postData)
+    //     }
+    // };
+    //
+    // const req = https.request(options, (res) => {
+    //     res.on('data', (d) => {
+    //         process.stdout.write(d)
+    //     })
+    // })
+    //
+    // req.on('error', (e) => {
+    //     console.log('httpsPostData error', e);
+    // })
+    //
+    // req.write(postData)
+    // req.end()
+
+    const postData = JSON.stringify(body);
+
     const options = {
         hostname: 'bronkz.app',
         port: 443,
@@ -52,22 +78,29 @@ const httpsPostData = (url, body) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(postData)
-        }
+            'Content-Length': Buffer.byteLength(postData),
+        },
     };
 
     const req = https.request(options, (res) => {
-        res.on('data', (d) => {
-            process.stdout.write(d)
-        })
-    })
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
 
     req.on('error', (e) => {
-        console.log('httpsPostData error', e);
-    })
+        console.error(`problem with request: ${e.message}`);
+    });
 
-    req.write(postData)
-    req.end()
+// Write data to request body
+    req.write(postData);
+    req.end();
 }
 
 module.exports = { parseJson, httpsGetData, httpsPostData }
