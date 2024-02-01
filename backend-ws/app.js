5c7a00c355a8ea0ef41c686b64a2428da5c93caf
prosergeant <sberendeyev@gmail.com>
2024-02-01 15:25:10 +0600

    log for ws 9

--- backend-ws/app.js
+++ backend-ws/app.js
@@ -21,7 +21,7 @@ app.post('/users/send-set-money/',  (req, res, next) => {
 		}))
 	}
 
-	res.status(200).send(`ws req body id: ${id}, coins: ${coins}`)
+	res.status(200).send(`ws req body id: ${id}, coins: ${coins}, user.id :${user?.id}`)
 })
 
 
